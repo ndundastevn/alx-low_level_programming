3f386199bf2f42c128dace6c999ce0691a7844de
ndundastevn <ndundastevn@gmail.com>
2023-02-17 06:56:33 -0800

    print aplhabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: print the alphabet in lowercase always
+ * Return: 0 
+ */
+int main(void)
+{
+  int num;
+  for (num = 'a'; num <= 'z'; num++)
+    {
+	putchar(num);
+    }
+  putchar('\n');
+  return (0);
+}
