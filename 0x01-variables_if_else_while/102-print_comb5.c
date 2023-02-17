69358311bdba6c883310fdddcee9a2e137f8227b
ndundastevn <ndundastevn@gmail.com>
2023-02-17 08:48:42 -0800

    alx project

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - Entry point
- * Description: prints all possible combination of 2 digit number
- * Return: 0
+ * Description: prints all possible combination of 2-digit numbers
+ * Return: Always 0 (success)
  */
 int main(void)
 {
- int c, i, k, j;
+	int c, i, k, j;
 
 	for (c = 48; c <= 57; c++)
 	{
