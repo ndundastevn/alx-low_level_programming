d3506fe906008df0d18d6a622b2c522a37a9d98d
ndundastevn <ndundastevn@gmail.com>
2023-02-17 07:00:20 -0800

    print aplhabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - Entry point
  * Description: print the alphabet in lowercase always
  * Return: 0 
- */
+*/
 int main(void)
 {
-  int num;
-  for (num = 'a'; num <= 'z'; num++)
-    {
-	putchar(num);
-    }
-  putchar('\n');
-  return (0);
+int num;
+for (num = 'a'; num <= 'z'; num++)
+{
+putchar(num);
+}
+putchar('\n');
+return (0);
 }
