eecb068e3289b5f737656ec9abd8a7c00673462c
ndundastevn <ndundastevn@gmail.com>
2023-02-18 02:48:26 -0800

    alx project

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#include <unistd.h>
- 
+
 /**
- * main - Entry point for the program
- * Description: perform a series of actions
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
  * Return: 0
-*/
+ */
+
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle"); 
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
