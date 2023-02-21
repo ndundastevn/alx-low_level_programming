ca05dccf9aedd956433d03c7724798a889458789
ndundastevn <ndundastevn@gmail.com>
2023-02-21 07:09:16 -0800

    alx project

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <unistd.h>
  
 /**
- * main - Entry point for the program
- * Description: perform a series of actions
+ * main - Entry point 
+ * Description: print putchs
  * Return: 0
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+  return ("_putchar\n");
 }
