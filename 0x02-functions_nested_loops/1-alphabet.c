948c67b796fa271982e3c49e061d3c8c7d64d453
ndundastevn <ndundastevn@gmail.com>
2023-02-21 07:05:21 -0800

    alx project

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
- * print_alphabet 
+ * print_alphabet - Entry point
+ * Description: prints the alphabet
  * Return: void
  */
 void print_alphabet(void)
 {
-char letter = 'a';
-while (letter <= 'z')
-{
-_putchar(letter);
-letter++;
-}
-_putchar('\n');
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
 }
