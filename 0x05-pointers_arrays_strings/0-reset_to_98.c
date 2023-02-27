8cb324f3885645380c2b75b325421e1a2dbfb09f
ndundastevn <ndundastevn@gmail.com>
2023-02-27 05:34:28 -0800

    alx project

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,10 +2,10 @@
 /**
 *  reset_to_98 - takes the pointer and updates the value
 *  @n: int to check
-*  Owned By Bwave/Bright
-*  Return: 0 is success
+*  return: 0
 */
 
-void reset_to_98(int *n){
-  *n = 98;
+void reset_to_98(int *n)
+{
+	*n = 98;
 }
