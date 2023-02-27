8f243aab43ab6771628b1260e6a584bec663b70e
ndundastevn <ndundastevn@gmail.com>
2023-02-27 06:20:19 -0800

    alx project

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <string.h>
 /*
- *_strlen: returns the length of string
- *@s: character to check
- *Description: this will return the length of a string
- *return: success
+ * _strlen - return the length of a string
+ * @s: char to check
+ * Description: this will return the length of a string
+ * Return: 0 is success
  */
 
 int _strlen(char *s)
