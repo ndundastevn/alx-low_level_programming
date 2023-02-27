e2ee6cc048a65fc7f07ed99d54431b4a0391ac6f
ndundastevn <ndundastevn@gmail.com>
2023-02-27 05:31:03 -0800

    alx project

---
+++ 0x05-pointers_arrays_strings/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+ 
+/**
+ * main - Entry point for the program
+ * Description: perform a series of actions
+ * Return: 0
+*/
+int main(void)
+{
+
+  int n = 402;
+
+  reset_to_98(&n);
+  
+return (0);
+}
