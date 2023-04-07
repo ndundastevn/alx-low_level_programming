20a103fd4cffd81c241b6a510728fd6e3d8b339b
ndundastevn <ndundastevn@gmail.com>
2023-04-07 08:13:47 -0700

    alx project

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,6 +4,7 @@
  * get_bit - returns the value of a bit at a given index.
  * @n: checking bits
  * @index: the index of bit to  to check 
+ *
  * Return: the value of the bit at index or -1 if error occures
  */
 int get_bit(unsigned long int n, unsigned int index)
