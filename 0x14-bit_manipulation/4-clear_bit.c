69a3d66ae6dc2860889d0e045b308bc57dcb690b
ndundastevn <ndundastevn@gmail.com>
2023-04-08 23:10:45 -0700

    alx project

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -6,7 +6,6 @@
  * @index: the index starting from 0 of the bit you want to set
  * Return: 1 if it worked, or -1 if an error occurred
  */
-
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int ii;
