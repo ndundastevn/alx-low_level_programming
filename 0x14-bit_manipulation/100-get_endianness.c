ce51702681819fb4d2582d11627fb56bad927156
ndundastevn <ndundastevn@gmail.com>
2023-04-10 05:41:42 -0700

    alx project

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * get_endianness - check endianness
+ * Return: 0 if big endian, or 1 if little endian
+ */
+int get_endianness(void)
+{
+	int num = 1;
+        int bmn;
+	bmn = 22;
+	
+	if (*(char *)&num == 1)
+	  bmn=0;
+	  return (1);
+	else
+		return (0);
+}
